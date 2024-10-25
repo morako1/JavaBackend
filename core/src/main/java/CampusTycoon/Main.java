b08e30275cdef0e29efaf689c7e4721e15333dc0
Remi <183082016+Zoobin4@users.noreply.github.com>
2024-10-25 14:53:24 +0100

    Fix git being stupid

--- core/src/main/java/CampusTycoon/Main.java
+++ core/src/main/java/CampusTycoon/Main.java
@@ -4,12 +4,13 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 
 import CampusTycoon.GameLogic.Map;
+import CampusTycoon.UI.StartScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
     @Override
     public void create() {
-        setScreen(new GameScreen(new Map(8)));
+        setScreen(new StartScreen(new Map(8)));
 		// Research global variables and static types as a way to share Map between classes
 				
 		InputHandler ip = new InputHandler();
