21b0c15c0edc34ce4f7e62d6cadd510bc3ae2edd
Remi <183082016+Zoobin4@users.noreply.github.com>
2024-11-06 00:09:49 +0000

    Update MenuText constructor to be more accurate

--- core/src/main/java/CampusTycoon/UI/Components/MenuText.java
+++ core/src/main/java/CampusTycoon/UI/Components/MenuText.java
@@ -6,8 +6,8 @@ import CampusTycoon.UI.Component;
 
 public class MenuText extends Component {
 
-	public MenuText(String text, float X, float Y, float Width, float Height) {
-		super(X, Y, Width, Height);
+	public MenuText(String text, float X, float Y, float WidthScale, float HeightScale) {
+		super(X, Y, WidthScale, HeightScale);
 		
 		this.text = text;
 		this.isText = true;
